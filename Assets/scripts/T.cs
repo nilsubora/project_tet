495476eb3355e0c6b1437a258171eeb1bb0909c1
nilsubora <boranilsu@gmail.com>
2023-01-10 01:36:35 +0300

    deletingrowdone

--- Assets/scripts/T.cs
+++ Assets/scripts/T.cs
@@ -8,7 +8,7 @@ public class T : MonoBehaviour
 
     private float previousTime;
     public float fallTime = 0.8f;
-    public static int heigth = 20;
+    public static int height = 20;
     public static int width = 10;
 
     public Vector3 rotationpoint;
@@ -17,7 +17,7 @@ public class T : MonoBehaviour
     {
 
     }
-    private static Transform[,] grid = new Transform[width, heigth];
+    private static Transform[,] grid = new Transform[width+1, height+1];
     void Addtogrid()
     {
         foreach (Transform children in transform)
@@ -52,10 +52,63 @@ public class T : MonoBehaviour
         this.enabled = false;
     }
 
-   
+    void Checklines()
+    {
+        for(int i = 0; i <= height - 1; i++)
+        //for(int i = height-1; i >= 0; i--)
+        {
+            if (LineExist(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+        
+        
+    }
+
+    bool LineExist(int i)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y=i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+
+
     void Update()
     {
-        
+        Checklines();
         if (Time.time - previousTime > ((Input.GetKeyDown(KeyCode.DownArrow))?fallTime/10:fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
@@ -64,7 +117,6 @@ public class T : MonoBehaviour
             {
                 transform.position -= new Vector3(0, -1, 0);
                 Addtogrid();
-            //    Debug.Log(grid);
                 Invoke("Stoptheprevious", 3.0f);
                 this.enabled = false;
                 FindObjectOfType<spawnpoint>().newblock();
